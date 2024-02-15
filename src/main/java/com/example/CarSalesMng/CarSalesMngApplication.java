2822c5d7cb3d7bc9b9c4555bfcd87e54a220f562
Lia Araruna <liaararuna@gmail.com>
2024-02-15 11:19:09 +0000

    Initial project file

---
+++ src/main/java/com/example/CarSalesMng/CarSalesMngApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarSalesMng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalesMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSalesMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarSalesMng/CarSalesMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarSalesMng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSalesMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
